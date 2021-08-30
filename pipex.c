3ddb192c060bc3a9b22cb46f1df35e144e67794a
toradziro <toradziro@gmail.com>
2021-08-30 21:07:09 +0300

    with execve flags

--- pipex.c
+++ pipex.c
@@ -1,10 +1,24 @@
 #include "pipex.h"
 
-pid_t	executor(t_pipex *pipex, int32_t *pipefd, int8_t flag, char **envp)
+int32_t wait_proccess(pid_t pid, t_pipex *pipex)
+{
+	int32_t status;
+
+	status = 0;
+	if (pid > 0)
+		status = waitpid(pid, NULL, WUNTRACED | WNOHANG | WCONTINUED);
+	if (pid < 0)
+		handle_errors(pipex);
+	return (status);
+}
+
+int32_t executor(t_pipex *pipex, int32_t *pipefd, int8_t flag, char **envp)
 {
 	pid_t	child;
+	int32_t	res;
 
 	child = fork();
+	res = 0;
 	if (child == 0)
 	{
 		if (flag == 0)
@@ -12,7 +26,8 @@ pid_t	executor(t_pipex *pipex, int32_t *pipefd, int8_t flag, char **envp)
 			close(pipefd[0]);
 			dup2(pipex->fd_1_file, STDIN_FILENO);
 			dup2(pipefd[1], STDOUT_FILENO);
-			execve(pipex->cmd_1, pipex->cmd_1_arg, envp);
+			if (execve(pipex->cmd_1, pipex->cmd_1_arg, envp) == -1)
+				handle_errors(pipex);
 			exit(errno);
 		}
 		if (flag == 1)
@@ -20,25 +35,33 @@ pid_t	executor(t_pipex *pipex, int32_t *pipefd, int8_t flag, char **envp)
 			close(pipefd[1]);
 			dup2(pipefd[0], STDIN_FILENO);
 			dup2(pipex->fd_2_file, STDOUT_FILENO);
-			execve(pipex->cmd_2, pipex->cmd_2_arg, envp);
+			if (execve(pipex->cmd_2, pipex->cmd_2_arg, envp) == -1)
+				handle_errors(pipex);
 			exit(errno);
 		}
 	}
-	else if (child == -1)
-		handle_errors(pipex);
-	return (child);
+	res = wait_proccess(child, pipex);
+	return (res);
 }
 
-void	start_pipex(t_pipex *pipex, int32_t *pipefd, char **envp)
+int32_t	start_pipex(t_pipex *pipex, int32_t *pipefd, char **envp)
 {
-	executor(pipex, pipefd, 0, envp);
-	executor(pipex, pipefd, 1, envp);
+	int32_t	res;
+
+	res = 0;
+	if (!pipex->is_first_cmd)
+		executor(pipex, pipefd, 0, envp);
+	if (!pipex->is_second_cmd)
+		res = executor(pipex, pipefd, 1, envp);
+	return (res);
 }
 
-void	init_pipex(char **argv, t_pipex *pipex, char **envp)
+int32_t init_pipex(char **argv, t_pipex *pipex, char **envp)
 {
 	int32_t	*pipefd;
+	int32_t	res;
 
+	res = 0;
 	find_path(envp, pipex);
 	pipex->cmd_1 = strdup(argv[2]);
 	pipex->cmd_2 = strdup(argv[3]);
@@ -48,24 +71,28 @@ void	init_pipex(char **argv, t_pipex *pipex, char **envp)
 	pipefd = (int32_t *)calloc(sizeof(int32_t), 2);
 	if (!pipefd)
 		handle_errors(pipex);
-	if (find_prog(pipex, &pipex->cmd_1) || find_prog(pipex, &pipex->cmd_2))
+	pipex->is_first_cmd = find_prog(pipex, &pipex->cmd_1);
+	pipex->is_second_cmd = find_prog(pipex, &pipex->cmd_2);
+	if (pipex->is_first_cmd && pipex->is_second_cmd)
 		handle_errors(pipex);
 	open_files(pipex);
 	pipe(pipefd);
-	start_pipex(pipex, pipefd, envp);
+	res = start_pipex(pipex, pipefd, envp);
 	free(pipefd);
+	return (res);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	*pipex;
+	int32_t	res;
 
 	pipex = (t_pipex *)calloc(sizeof(t_pipex), 1);
 	if (!pipex)
 		handle_errors(pipex);
 	if (argc != 5)
 		handle_errors(pipex);
-	init_pipex(argv, pipex, envp);
+	res = init_pipex(argv, pipex, envp);
 	free_pipe(pipex);
-	return (0);
+	return (res);
 }
--- pipex.h
+++ pipex.h
@@ -9,12 +9,6 @@
 # include <stdlib.h>
 # include <errno.h>
 
-# define MALLOC_ERROR -1
-# define WRONG_ARGS_AMOUNT -2
-# define WRONG_FILES -3
-# define WRONG_PROG -4
-# define FATAL_ERROR -5
-
 typedef struct s_pipex {
 	char	**cmd_1_arg;
 	char	**cmd_2_arg;
@@ -25,6 +19,8 @@ typedef struct s_pipex {
 	char	*file2;
 	int32_t	fd_1_file;
 	int32_t	fd_2_file;
+	int8_t	is_first_cmd;
+	int8_t	is_second_cmd;
 }				t_pipex;
 
 char	**ft_split(char *s, char c);
@@ -36,7 +32,7 @@ int8_t	find_space(char *str);
 void	parse_prog_args(t_pipex *pipex);
 void	handle_errors(t_pipex *pipex);
 void	open_files(t_pipex *pipex);
-void	start_pipex(t_pipex *pipex, int32_t *pipefd, char **envp);
+int32_t	start_pipex(t_pipex *pipex, int32_t *pipefd, char **envp);
 void	free_pipe(t_pipex *pipex);
 void	free_db_arr(char **arr);
 
